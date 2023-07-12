ef71b4362b1fb7ff8cdbbb0bfc1f1c78ca4647b4
zpf <2384085701@qq.com>
2023-07-12 19:28:25 +0800

    first commit

---
+++ utils/currying/index.js
@@ -0,0 +1,17 @@
+/**
+ * @description A function that curries a function
+ * @param {*} func Functions that need to be curried
+ * @returns Return a curried function
+ */
+function currying(func) {
+  function curryFn(...args) {
+    if (args.length >= func.length) {
+      func(...args)
+    } else {
+      return function (...newArgs) {
+        return curryFn(...args.concat(newArgs))
+      }
+    }
+  }
+  return curryFn
+}
